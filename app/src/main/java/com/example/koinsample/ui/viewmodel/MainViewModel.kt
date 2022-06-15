fb6e1870b02623a7f78f010bd39a32a28925a040
Semir Hodzic <semir.hodzic@mobileinteraction.se>
2022-06-15 13:43:30 +0200

    Add viewmodel to handle ui related data

---
+++ app/src/main/java/com/example/koinsample/ui/viewmodel/MainViewModel.kt
@@ -0,0 +1,39 @@
+package com.example.koinsample.ui.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.koinsample.data.dto.WeatherForecast
+import com.example.koinsample.data.repository.WeatherRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.launch
+
+class MainViewModel(
+    private val repository: WeatherRepository,
+    private val ioDispatcher: CoroutineDispatcher,
+) : ViewModel() {
+
+    private val _forecast = MutableLiveData<WeatherForecast?>()
+    val forecast: LiveData<WeatherForecast?> = _forecast
+
+    private val _error = MutableLiveData<String>()
+
+
+    fun getWeatherFor(city: String) {
+        viewModelScope.launch(ioDispatcher) {
+            try {
+                val response = repository.getWeatherFor(city = city)
+                val weatherForecast = response.body()
+
+                if (response.isSuccessful && weatherForecast != null) {
+                    _forecast.postValue(weatherForecast)
+                } else {
+                    _error.postValue("Error: HttpCode(${response.code()})")
+                }
+            } catch (t: Throwable) {
+                _error.postValue(t.message)
+            }
+        }
+    }
+}
