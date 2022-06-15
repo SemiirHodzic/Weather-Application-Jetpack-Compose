f0e8dd6ffeb18d0abc7fb03245d53fe671198eda
Semir Hodzic <semir.hodzic@mobileinteraction.se>
2022-06-15 15:00:18 +0200

    Add helper function for navigating with arguments

--- app/src/main/java/com/example/koinsample/ui/main/Screen.kt
+++ app/src/main/java/com/example/koinsample/ui/main/Screen.kt
@@ -3,4 +3,13 @@ package com.example.koinsample.ui.main
 sealed class Screen(val route: String) {
     object ShowForecast : Screen("/show_forecast_screen")
     object SelectCity : Screen("/select_city_screen")
+
+    fun navigateWithArguments(vararg args: String): String {
+        return buildString {
+            append(route)
+            args.forEach { arg ->
+                append("/$arg")
+            }
+        }
+    }
 }
