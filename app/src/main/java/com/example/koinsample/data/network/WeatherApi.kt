57ffa159fb33491b1ce20809c850e552f0320df0
Semir Hodzic <semir.hodzic@mobileinteraction.se>
2022-06-15 13:46:10 +0200

    Add interface for GET function

---
+++ app/src/main/java/com/example/koinsample/data/network/WeatherApi.kt
@@ -0,0 +1,15 @@
+package com.example.koinsample.data.network
+
+import com.example.koinsample.data.dto.WeatherForecast
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherApi {
+    @GET("data/2.5/forecast/")
+    suspend fun getCityData(
+        @Query("q") q: String,
+        @Query("units") units: String,
+        @Query("appid") appId: String
+    ): Response<WeatherForecast>
+}
