95e7696a5d45704cf07c0453b7a99ff28515f61f
Semir Hodzic <semir.hodzic@mobileinteraction.se>
2022-06-15 14:29:54 +0200

    Add string argument for navigation between SelectCityScreen.kt and ShowForecastScreen.kt

--- app/src/main/java/com/example/koinsample/ui/screens/MainScreen.kt
+++ app/src/main/java/com/example/koinsample/ui/screens/MainScreen.kt
@@ -1,9 +1,11 @@
 package com.example.koinsample.ui.screens
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.koinsample.ui.main.Screen
 import com.example.koinsample.ui.viewmodel.MainViewModel
 
@@ -15,13 +17,21 @@ fun MainScreen(viewModel: MainViewModel) {
         composable(
             route = Screen.SelectCity.route,
         ) {
-            SelectCityScreen()
+            SelectCityScreen(navController)
         }
 
         composable(
-            route = Screen.ShowForecast.route
+            route = Screen.ShowForecast.route + "/{city}",
+            arguments = listOf(
+                navArgument("city") {
+                    type = NavType.StringType
+                    defaultValue = "Stockholm"
+                    nullable = true
+                }
+            )
         ) {
             ShowForecastScreen(
+                city = it.arguments?.getString("city"),
                 viewModel = viewModel,
             )
         }
