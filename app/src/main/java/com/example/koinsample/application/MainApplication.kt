23d951306d6d1dc4e408b56c398ac34f2a512203
Semir Hodzic <semir.hodzic@mobileinteraction.se>
2022-06-17 10:47:33 +0200

    Rename package

--- app/src/main/java/com/example/koinsample/application/MainApplication.kt
+++ app/src/main/java/com/example/koinsample/application/MainApplication.kt
@@ -1,7 +1,7 @@
-package com.example.koinsample.ui.main
+package com.example.koinsample.application
 
 import android.app.Application
-import com.example.koinsample.appModule
+import com.example.koinsample.di.appModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
