742cefc1481a3b223d465a559f87d5b8e98a2231
Semir Hodzic <semir.hodzic@mobileinteraction.se>
2022-06-17 10:48:56 +0200

    Add preview for response in composable function

--- app/src/main/java/com/example/koinsample/ui/screens/ShowForecastScreen.kt
+++ app/src/main/java/com/example/koinsample/ui/screens/ShowForecastScreen.kt
@@ -1,6 +1,6 @@
 package com.example.koinsample.ui.screens
 
-import android.util.Log
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -11,11 +11,14 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.koinsample.R
 import com.example.koinsample.data.dto.Forecast
+import com.example.koinsample.ui.theme.KoinSampleTheme
 import com.example.koinsample.ui.viewmodel.MainViewModel
 import com.example.koinsample.utils.fromApiTime
+import com.example.koinsample.utils.loadPreviewResponse
 import com.example.koinsample.utils.timeFormatHoursMinutes
 import org.joda.time.DateTime
 
@@ -75,8 +78,9 @@ fun ForecastDayCard(
         Row(
             modifier = Modifier
                 .fillMaxWidth()
+                .height(100.dp)
                 .padding(8.dp),
-            horizontalArrangement = Arrangement.SpaceAround,
+            horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically,
         ) {
             val date = forecast.dt.fromApiTime()
@@ -108,5 +112,16 @@ fun SimpleTempDisplay(forecast: Forecast) {
             ),
             style = MaterialTheme.typography.body1
         )
+    }
+}
+
+@Preview("Forecast screen")
+@Preview("Forecast screen darkMode", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun PreviewForecastScreen() {
+    KoinSampleTheme() {
+        Scaffold {
+            ForecastList(loadPreviewResponse().list)
+        }
     }
 }
