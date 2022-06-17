6cdaa5b9300197ba0d94a4dae40324b2552bba8c
Semir Hodzic <semir.hodzic@mobileinteraction.se>
2022-06-17 10:50:14 +0200

    Add constant

--- app/src/main/java/com/example/koinsample/repository/WeatherRepositoryImpl.kt
+++ app/src/main/java/com/example/koinsample/repository/WeatherRepositoryImpl.kt
@@ -1,13 +1,14 @@
-package com.example.koinsample.data.repository
+package com.example.koinsample.repository
 
 import com.example.koinsample.data.dto.WeatherForecast
 import com.example.koinsample.data.network.WeatherApi
 import retrofit2.Response
+import com.example.koinsample.utils.metricUnits
 
 class WeatherRepositoryImpl(
     private val api: WeatherApi
 ) : WeatherRepository {
     override suspend fun getWeatherFor(city: String?): Response<WeatherForecast> {
-        return api.getCityData(city, "metric", "1487ee4774bdde15c075dab2c6f5ef13")
+        return api.getCityData(city, metricUnits, "1487ee4774bdde15c075dab2c6f5ef13")
     }
 }
