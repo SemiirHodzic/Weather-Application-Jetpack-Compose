f98465efa78a08f55cb242faf33d097dfef0b534
Semir Hodzic <semir.hodzic@mobileinteraction.se>
2022-06-20 09:25:05 +0200

    Add new screen in navigation helper class

--- app/src/main/java/com/example/koinsample/ui/main/Screen.kt
+++ app/src/main/java/com/example/koinsample/ui/main/Screen.kt
@@ -2,7 +2,7 @@ package com.example.koinsample.ui.main
 
 sealed class Screen(val route: String) {
     object ShowForecast : Screen("/show_forecast_screen")
-    object SelectCity : Screen("/select_city_screen")
+    object SearchCity : Screen("/select_city_screen")
 
     fun navigateWithArguments(vararg args: String): String {
         return buildString {
