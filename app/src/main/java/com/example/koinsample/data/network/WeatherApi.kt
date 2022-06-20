c6e5948cc15ccb8ff31e4972ffb094ebae128299
Semir Hodzic <semir.hodzic@mobileinteraction.se>
2022-06-20 09:27:21 +0200

    Rename package

--- app/src/main/java/com/example/koinsample/data/network/WeatherApi.kt
+++ app/src/main/java/com/example/koinsample/data/network/WeatherApi.kt
@@ -1,13 +1,13 @@
 package com.example.koinsample.data.network
 
-import com.example.koinsample.data.dto.WeatherForecast
+import com.example.koinsample.data.dto.forecast.WeatherForecast
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface WeatherApi {
     @GET("data/2.5/forecast/")
-    suspend fun getCityData(
+    suspend fun getCityWeather(
         @Query("q") q: String?,
         @Query("units") units: String,
         @Query("appid") appId: String
