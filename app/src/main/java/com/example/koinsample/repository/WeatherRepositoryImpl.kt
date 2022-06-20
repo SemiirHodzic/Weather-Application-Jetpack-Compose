ab568593c9e4dc48e621dd042fc93a51afc1a9ed
Semir Hodzic <semir.hodzic@mobileinteraction.se>
2022-06-20 09:28:25 +0200

    Add function for getting list of cities from api

--- app/src/main/java/com/example/koinsample/repository/WeatherRepositoryImpl.kt
+++ app/src/main/java/com/example/koinsample/repository/WeatherRepositoryImpl.kt
@@ -1,14 +1,21 @@
 package com.example.koinsample.repository
 
-import com.example.koinsample.data.dto.WeatherForecast
+import com.example.koinsample.data.dto.city.CityCollection
+import com.example.koinsample.data.dto.forecast.WeatherForecast
+import com.example.koinsample.data.network.CityApi
 import com.example.koinsample.data.network.WeatherApi
 import retrofit2.Response
 import com.example.koinsample.utils.metricUnits
 
 class WeatherRepositoryImpl(
-    private val api: WeatherApi
+    private val api: WeatherApi,
+    private val api2: CityApi
 ) : WeatherRepository {
     override suspend fun getWeatherFor(city: String?): Response<WeatherForecast> {
-        return api.getCityData(city, metricUnits, "1487ee4774bdde15c075dab2c6f5ef13")
+        return api.getCityWeather(city, metricUnits, "1487ee4774bdde15c075dab2c6f5ef13")
+    }
+
+    override suspend fun getCitiesFromQuery(city: String): Response<CityCollection> {
+        return api2.getCityFromQuery(city, "1487ee4774bdde15c075dab2c6f5ef13")
     }
 }
